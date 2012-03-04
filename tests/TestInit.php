86c480de76fb2e0eeab976ae462b7e72c9259e5d
Benjamin Eberlei <kontakt@beberlei.de>
2012-03-04 17:36:31 +0100

    Initial move from Doctrine/CouchDB namespace into its own subproject.

---
+++ tests/TestInit.php
@@ -0,0 +1,21 @@
+<?php
+// Tests/bootstrap.php
+$loader = @include __DIR__ . '/../vendor/.composer/autoload.php';
+if (!$loader) {
+    die(<<<'EOT'
+You must set up the project dependencies, run the following commands:
+wget http://getcomposer.org/composer.phar
+php composer.phar install
+EOT
+    );
+}
+
+use Doctrine\Common\ClassLoader;
+
+$classLoader = new ClassLoader('Doctrine\Tests', __DIR__ . '/../tests');
+$classLoader->register();
+
+Doctrine\Common\Annotations\AnnotationRegistry::registerAutoloadNamespace(
+    'Doctrine\ODM\CouchDB\Mapping\Annotations', __DIR__ . '/../lib'
+);
+
