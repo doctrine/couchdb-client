64cd77fdbbe66fd8abe394500fa940398253609d
Patrick Romowicz <patrick@romowicz.de>
2013-09-05 13:34:00 +0200

    Update CouchDBClient.php

--- lib/Doctrine/CouchDB/CouchDBClient.php
+++ lib/Doctrine/CouchDB/CouchDBClient.php
@@ -35,9 +35,9 @@ use Doctrine\CouchDB\View\DesignDocument;
  */
 class CouchDBClient
 {
-    
+    /** string \ufff0 */
     const COLLATION_END = "\xEF\xBF\xB0";
-    
+
     /**
      * Name of the CouchDB database
      *
