2d3e9d2a9dc9cf93fd5a72bb8aed23558ca080b8
Vladimir Hidalgo <vladimiroski@gmail.com>
2020-07-28 16:44:44 -0600

    Update CouchDBClient.php
    On clustered CouchDB servers deleting a DB may return 202 - Accepted: Request has been accepted, but the corresponding operation may not have completed.
    
    Reference: https://docs.couchdb.org/en/stable/api/basics.html#http-status-codes

--- lib/Doctrine/CouchDB/CouchDBClient.php
+++ lib/Doctrine/CouchDB/CouchDBClient.php
@@ -360,7 +360,7 @@ class CouchDBClient
     {
         $response = $this->httpClient->request('DELETE', '/'.urlencode($name));
 
-        if ($response->status != 200 && $response->status != 404) {
+        if (!(in_array($response->status, [200,202,404]))) {
             throw HTTPException::fromResponse('/'.urlencode($name), $response);
         }
     }
